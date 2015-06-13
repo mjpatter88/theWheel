b1d1b008a3de2f8cdccf7993a34eddf73c575e88
Michael Patterson <mjpatter88@gmail.com>
2015-06-13 16:55:18 -0400

    Started pathfinding project. JB IDE ignore added.

---
+++ pathfinding/src/Main.java
@@ -0,0 +1,11 @@
+/**
+ *
+ * Created by Michael on 6/13/2015.
+ */
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Test Pathfinding Project.");
+    }
+}
