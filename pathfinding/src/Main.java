adae785be152bc1334cad3fce41c80c73438af13
Michael Patterson <mjpatter88@gmail.com>
2015-06-15 01:34:04 -0400

    Made this project a basic Java FX application. Right now it just opens a window with a button that prints "Hello World" when clicked.

--- pathfinding/src/Main.java
+++ pathfinding/src/Main.java
@@ -9,10 +9,39 @@
  * Djikstra's
  *
  */
-public class Main
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application
 {
-    public static void main(String[] args)
-    {
-        System.out.println("Test Pathfinding Project.");
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 250);
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
