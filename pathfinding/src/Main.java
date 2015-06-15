faaae13bc4f1cd449c9434d3c7aaa51beaf9fb55
Michael Patterson <mjpatter88@gmail.com>
2015-06-14 23:52:49 -0400

    Added a brief description of what this project aims to do.

--- pathfinding/src/Main.java
+++ pathfinding/src/Main.java
@@ -1,9 +1,16 @@
 /**
  *
  * Created by Michael on 6/13/2015.
+ *
+ * This project implements several path-finding algorithms and an interface to use them.
+ *
+ * Algorithms:
+ * A*
+ * Djikstra's
+ *
  */
-public class Main {
-
+public class Main
+{
     public static void main(String[] args)
     {
         System.out.println("Test Pathfinding Project.");
